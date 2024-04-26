24b709889c1596a6c990a0d4894ae5f047870f38
Trusha Shah <trushashah1402@gmail.com>
2024-04-26 15:37:06 +0530

    change in server.js

--- server/server.js
+++ server/server.js
@@ -13,15 +13,15 @@ app.use("/api/users", usersRoute);
 app.use("/api/inventory", inventoryRoute);
 app.use("/api/dashboard", dashboardRoute);
 
-// deployment config
-const path = require("path");
-__dirname = path.resolve();
+// // deployment config
+// const path = require("path");
+// __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/client/build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 app.listen(port, () => console.log(`Node JS Server Started at ${port}`));
